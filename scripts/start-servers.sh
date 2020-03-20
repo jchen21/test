5cb454977488b845b766941ba21ce15f5d9d96ae
Jianxia Chen <jchen21@apache.org>
2020-03-19 18:19:35 -0700

    Fix another bug
    Authored-by: Jianxia Chen <jchen21@apache.org>

--- scripts/start-servers.sh
+++ scripts/start-servers.sh
@@ -5,7 +5,7 @@ set -x
 # start servers
 for i in {1..2} 
 do
-  gcloud compute ssh geode-vm-${i} --command "~/test/scripts/start-server.sh {i}"
+  gcloud compute ssh geode-vm-${i} --command "~/test/scripts/start-server.sh ${i}" &
 done
 
 wait
