f6f197f7bc5bf424a01f459622d7c64d5bb654f8
Jianxia Chen <jchen21@apache.org>
2020-03-22 15:43:25 -0700

    Add GC flags
    Authored-by: Jianxia Chen <jchen21@apache.org>

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 set -x
 
 # start server
-~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "start server --name=server${1} --locators=geode-vm-0.c.data-g2c.internal[10334] --initial-heap=20G --max-heap=20G"
+~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "start server --name=server${1} --locators=geode-vm-0.c.data-g2c.internal[10334] --initial-heap=20G --max-heap=20G --J=-XX:+UseConcMarkSweepGC --J=-XX:+CMSClassUnloadingEnabled --J=-XX:CMSInitiatingOccupancyFraction=75 --J=-XX:+PrintGCDetails --J=-XX:+PrintGCDateStamps"
 
 
