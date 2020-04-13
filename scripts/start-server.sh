229698f07f1f6baaa3445ae1bece2ed64ee5f56b
Jianxia Chen <jchen21@apache.org>
2020-04-13 12:26:38 -0700

    Add server number to gc log file name
    Authored-by: Jianxia Chen <jchen21@apache.org>

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 set -x
 
 # start server
-~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "start server --name=server${1} --locators=geode-vm-0.c.data-g2c.internal[10334] --J=-Dgemfire.statistic-archive-file=server${1}.gfs --initial-heap=100G --max-heap=100G --J=-XX:+UseConcMarkSweepGC --J=-XX:+CMSClassUnloadingEnabled --J=-XX:CMSInitiatingOccupancyFraction=75 --J=-Xloggc:gc.log --J=-XX:+PrintGCDetails --J=-XX:+PrintGCDateStamps --J=-XX:+PrintGCTimeStamps --J=-XX:+PrintTenuringDistribution"
+~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "start server --name=server${1} --locators=geode-vm-0.c.data-g2c.internal[10334] --J=-Dgemfire.statistic-archive-file=server${1}.gfs --initial-heap=100G --max-heap=100G --J=-XX:+UseConcMarkSweepGC --J=-XX:+CMSClassUnloadingEnabled --J=-XX:CMSInitiatingOccupancyFraction=75 --J=-Xloggc:gc${1}.log --J=-XX:+PrintGCDetails --J=-XX:+PrintGCDateStamps --J=-XX:+PrintGCTimeStamps --J=-XX:+PrintTenuringDistribution"
 
 
