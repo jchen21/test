ea2f0e61a41f006d9aba1d49bd9c33600aeea058
Jianxia Chen <11181423+jchen21@users.noreply.github.com>
2020-09-25 18:34:13 -0700

    Use default number of buckets

--- scripts/create-2-regions-separate-diskstores.sh
+++ scripts/create-2-regions-separate-diskstores.sh
@@ -7,6 +7,6 @@ set -x
 #~/workspace/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect --locator=localhost[10334]" -e "create disk-store --name=diskstore1 --dir=diskstore1" -e "create disk-store --name=diskstore2 --dir=diskstore2"
 
 # create two regions using different disk stores
-~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect --locator=geode-vm-0.c.data-g2c.internal[10334]" -e "create region --name=testRegion1 --type=PARTITION_REDUNDANT_PERSISTENT --total-num-buckets=2753 --disk-store=diskstore1" -e "create region --name=testRegion2 --type=PARTITION_REDUNDANT_PERSISTENT --total-num-buckets=2753 --disk-store=diskstore2"
+~/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect --locator=geode-vm-0.c.data-g2c.internal[10334]" -e "create region --name=testRegion1 --type=PARTITION_REDUNDANT_PERSISTENT --disk-store=diskstore1" -e "create region --name=testRegion2 --type=PARTITION_REDUNDANT_PERSISTENT --disk-store=diskstore2"
 #~/workspace/geode/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect --locator=localhost[10334]" -e "create region --name=testRegion1 --type=PARTITION_REDUNDANT_PERSISTENT --disk-store=diskstore1" -e "create region --name=testRegion2 --type=PARTITION_REDUNDANT_PERSISTENT --disk-store=diskstore2"
 
